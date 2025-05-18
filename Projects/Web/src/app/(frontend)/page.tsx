1b09e5c28e811d00807fb47693ff0aa84b9db3e5
Neonsy <118444485+Neonsy@users.noreply.github.com>
2025-05-18 22:05:03 +0200

    style: improve HomePage layout and styling
    Add padding to the main container for better spacing and center-align
    the heading text. Highlight key terms "HomePage" and "Next.JS" with
    bold and secondary color to enhance visual emphasis. These changes
    improve the overall presentation and readability of the homepage.

--- Projects/Web/src/app/(frontend)/page.tsx
+++ Projects/Web/src/app/(frontend)/page.tsx
@@ -5,8 +5,11 @@ export const metadata: Metadata = homeMetadata;
 
 export default function Home() {
     return (
-        <main className='flex h-[90svh] items-center justify-center'>
-            <h1>The HomePage will being worked on Next.JS ;)</h1>
+        <main className='flex h-[90svh] items-center justify-center px-12'>
+            <h1 className='text-center'>
+                The <span className='text-secondary font-bold'>HomePage</span> will being worked on{' '}
+                <span className='text-secondary font-bold'>Next.JS</span> 😜
+            </h1>
         </main>
     );
 }
